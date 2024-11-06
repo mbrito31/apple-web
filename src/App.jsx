bd5ddd9856bd3fd8f75583e52e38315fef3aa372
Matheus Brito <mbrito_hc@live.com>
2024-11-06 20:09:43 -0300

    elementos de cor

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights"; 
+import Model from "./components/Model"; 
+
 
 
 const App = () => {
@@ -9,6 +11,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   );
 };
