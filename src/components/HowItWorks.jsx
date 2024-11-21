33f07c3ff263bffdfb0301ab6052c86c725fd324
Matheus Brito <mbrito_hc@live.com>
2024-11-21 19:12:41 -0300

    new tools

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,8 +1,11 @@
-import { chipImg, frameImg } from '../utils';
+import { chipImg, frameImg, frameVideo } from '../utils';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+import { useRef } from 'react';
+import { animateWithGsap } from '../utils/animations';
 
 const HowItWorks = () => {
+  const videoRef = useRef();
   useGSAP(() => {
     gsap.from('#chip', {
       scrollTrigger: {
@@ -12,6 +15,13 @@ const HowItWorks = () => {
       opacity: 0,
       scale: 2,
       duration: 2,
+      ease: 'power2.inOut',
+    });
+
+    animateWithGsap('.g_fadeIn', {
+      opacity: 1,
+      y: 0,
+      duration: 1,
       ease: 'power2.inOut',
     });
   }, []);
@@ -40,6 +50,48 @@ const HowItWorks = () => {
                 className="bg-transparent relative z-10"
               />
             </div>
+            <div className="hiw-video">
+              <video
+                className="pointer-events-none"
+                playsInline
+                preload="none"
+                muted
+                autoPlay
+                ref={videoRef}
+              >
+                <source src={frameVideo} type="video/mp4" />
+              </video>
+            </div>
+          </div>
+
+          <p className="text-grey font-semibold text-center mt-3">
+            Honkai: Star Rail
+          </p>
+        </div>
+        <div className="hiw-text-container">
+          <div className="flex flex-1 justify-center flex-col">
+            <p className="hiw-text g_fadeIn">
+              A17 Pro is an entirely new class of iPhone chipthat delivers our{' '}
+              <span className="text-white">
+                best graphic performance by far
+              </span>
+              .
+            </p>
+          
+
+          <p className="hiw-text g_fadeIn">
+            Mobile{' '}
+            <span className="text-white">
+              games will look and feel so immersive
+            </span>
+            , with incredibly detailed environments and characters.
+          </p>
+          </div>
+
+          <div className="flex-1 flex justify-center flex-col g_fadeIn">
+            <p className="hiw-text">New</p>
+            <p className="hiw-bigtext">Pro-class GPU</p>
+            <p className="hiw-text">with 6 cores</p>
           </div>
         </div>
       </div>
